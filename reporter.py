6f92f5c0c093df5f5941254b0c3f61d08ea59f17
AkashPanwar08 <71636931+AkashPanwar08@users.noreply.github.com>
2024-07-15 20:27:44 +0530

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -121,6 +121,24 @@ if __name__ == "__main__":
                     print(f"Error in generating ElasticIp report: {e}")
 
 
-MJ_APIKEY_PUBLIC=e4ef8b25dd289a37d1b26460e89c1c52
-MJ_APIKEY_PRIVATE=5f4d11d663fc8fe5131df3abbe56e47b
-MJ_SENDER_EMAIL=mangesh.shinde@hexaviewtech.com
+"us-east-1",
+      "us-east-2",
+      "us-west-1",
+      "us-west-2",
+      "af-south-1",
+      "ap-east-1",
+      "ap-south-1",
+      "ap-northeast-3",
+      "ap-northeast-2",
+      "ap-southeast-1",
+      "ap-southeast-2",
+      "ap-northeast-1",
+      "ca-central-1",
+      "eu-central-1",
+      "eu-west-1",
+      "eu-west-2",
+      "eu-south-1",
+      "eu-west-3",
+      "eu-north-1",
+      "me-south-1",
+      "sa-east-1"
